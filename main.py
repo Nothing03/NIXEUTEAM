d03f4a47252eccd8dbc8b3df6c1924ff1213e989
NIXEUTEAM <120196346+Nothing03@users.noreply.github.com>
2022-12-18 23:39:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ from core import (
 
 REPO = """
 🤖 **Music Player**
-- Repo: [GitHub](https://github.com/Nothing03/Artemide)
+- Repo: [GitHub](https://github.com/Nothing03/NIXEU_MUSIC)
 - License: AGPL-3.0-or-later
 """
 
